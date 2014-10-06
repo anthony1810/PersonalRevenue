2086c7064e8c59bc1a8c36313da4f3b49c1f8ee0
Nhat Quang <qquang269@gmail.com>
2014-10-06 09:13:51 +0700

    First commit

---
+++ Personal RevenueTests/Personal_RevenueTests.m
@@ -0,0 +1,34 @@
+//
+//  Personal_RevenueTests.m
+//  Personal RevenueTests
+//
+//  Created by FCS on 10/6/14.
+//  Copyright (c) 2014 nhatquang@floatingcube.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Personal_RevenueTests : XCTestCase
+
+@end
+
+@implementation Personal_RevenueTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
